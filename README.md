2218fb7968bcaa2b879b104eec71a8e7cca35978
UnDuhDuhSea <tafimba@gmail.com>
2021-03-25 18:40:55 -0700

    updated README, fixed color change function, added localstorage

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Work-Day-Scheduler
+
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+![WorkDayScheduler](assets\imgs\WorkDaySchedule.jpg "ScheduleApp")
+
+## About The Project
+
+This project is a calendar app that allows you to save your tasks in the text area provided for each hour of a workday. The color formatting will change based off of what time of day it is (Grey for past, red for the current hour, and green for future hours). All of the date you save in the text area boxes is saved in local storage, which allows you to close out the calendar and open it back up with all of your tasks still populating the hour boxes.
+
+### Built With
+
+    -HTML
+    -CSS
+    -JavaScript
+    -jQuery
+    -Bootstrap
+
+## Contact
+
+Tyler Abegg - [@SeaDuh](https://twitter.com/SeaDuh) - tafimba@gmail.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: www.linkedin.com/in/tyler-abegg
+[product-screenshot]: images/screenshot.png
--- script.js
+++ script.js
@@ -1,52 +1,34 @@
-var hour9 = document.getElementById('hour-9').children[1].text;
-console.log(hour9)
-var btn1 = document.getElementById('btn1')
-var txt1 = document.getElementById('txt1');
-var hour10 = document.getElementById('hour-10')
-var hour11 = document.getElementById('hour-11')
-var hour12 = document.getElementById('hour-12')
-var hour13 = document.getElementById('hour-13')
-var hour14 = document.getElementById('hour-14')
-var hour15 = document.getElementById('hour-15')
-var hour16 = document.getElementById('hour-16')
-var hour17 = document.getElementById('hour-17')
-var past = document.querySelector('.past')
-var present = document.querySelector('.present')
-var future = document.querySelector('.future')
-var saveBtn = document.querySelectorAll('.saveBtn');
 var currentHour = moment().format('h');
-console.log(currentHour)
-// jquery
 var saveBtns = $('.saveBtn')
+var hourArr = $('.time-block')
+
 // function to save text in box to local storage 
+
 saveBtns.on('click', function (event) {
     var taskText = $(this).siblings('.description').val();
     console.log(taskText)
-    var parentID = $(this).parent().attr('id');
+    var parentID = $(this).parent("div").attr('id');
     localStorage.setItem(parentID, taskText);
-
-    storeTasks();
-    renderTasks();
+    displayTasks();
 });
 
+function displayTasks() {
+    for (var i = 9; i <= 17; i += 1) {
+        var storedStr = localStorage.getItem("hour-" + [i]);
+        console.log(storedStr)
+        $("#hour-" + [i] + " > textarea").val(storedStr);
+        console.log($("#hour-" + [i] + " > textarea"))
+    }
+}
 
 
-// ----------- Working for 1 text box saving ----------------------------
-// if (window.localStorage["taskText"]) {
-//     txt1.value = window.localStorage["taskText"];
-// }
-// txt1.addEventListener("keyup", function () {
-//     window.localStorage["taskText"] = txt1.value;
-// });
 
 
-// ----------------------------------------------------------------------
 
 // function for displaying current time on jumbotron
 function displayTime() {
     var headCurrentTime = moment().format('LLL');
-    var currentDay = document.getElementById('currentDay');
-    currentDay.textContent = headCurrentTime;
+    $('#currentDay').text(headCurrentTime);
 };
 
 setInterval(displayTime, 1000)
@@ -54,29 +36,56 @@ displayTime();
 
 // ######### page color change function ##########
 
-// function colorChange() {
-//     // if statement matching 
-//     console.log(currentHour)
-// }
-// colorChange();
+function colorChange() {
+    // var currentHour = moment().format("HH");
+    var currentHour = moment().hour();
+
+    for (var i = 9; i <= 17; i++) {
+        if (currentHour > [i]) {
+            $("#hour-" + [i]).addClass("past");
+        } else if (currentHour == [i]) {
+            $("#hour-" + [i]).addClass("present");
+        } else if (currentHour < [i]) {
+            $("#hour-" + [i]).addClass("future");
+        }
+    }
+}
+colorChange();
+displayTasks();
 
 
-// ############################################
-// Steve Idea
-// function renderSchedule() {
-//     var storageKey = hour10.attributes('id');
-//     var value = localStorage.getItem(storageKey)
-//     hour10.fine(".description").val(value);
-// }
 
-// // // store tasks in localstorage
-// function storeTasks(taskText) {
-//     localStorage.setItem("txt1", taskText);
-//     alert(taskText)
-// }
 
-// function renderTasks() {
-//     txt1.txtContent = localStorage.getItem("txt1")
-//     alert('renderTasks')
-// }
-// ###################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
